dfb5eb8c72ce6c20422635c39406ddb68e6ded5f
jokasta <jokasta.29@gmail.com>
2023-10-24 16:38:24 +0100

    angular-blog

---
+++ app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+  "id":"0",
+  "title": "The Marvels",
+  "description": "Carol Danvers AKA Captain Marvel has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan Kamala Khan, aka Ms. Marvel, and Carol’s estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau.",
+  "photo_Cover":"https://www.gamespot.com/a/uploads/original/1597/15975876/4191965-themarvels_27x40_imax_digital_supplemental_v3_lg.jpg",
+  "video_Content": "https://youtu.be/wS_qbDztgVY"
+  },
+  {
+    "id":"2",
+    "title": "Guardians of the Galaxy Vol. 3 ",
+    "description": "In Marvel Studios 'Guardians of the Galaxy Vol. 3' our beloved band of misfits are looking a bit different these days. Peter Quill, still reeling from the loss of Gamora, must rally his team around him to defend the universe along with protecting one of their own. A mission that, if not completed successfully, could quite possibly lead to the end of the Guardians as we know them.",
+    "photo_Cover":"https://lumiere-a.akamaihd.net/v1/images/p_guardiansofthegalaxy_vol3_2106_fe5dfa27.jpeg"
+  },
+{
+  "id":"3",
+  "title": "Ant-Man and the Wasp QUANTUMANIA",
+  "description": "In the film, which officially kicks off phase 5 of the Marvel Cinematic Universe, Super-Hero partners Scott Lang and Hope Van Dyne return to continue their adventures as Ant-Man and the Wasp. Together, with Hope's parents Hank Pym and Janet Van Dyne, the family finds themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that will push them beyond the limits of what they thought was possible. Jonathan Majors joins the adventure as Kang.",
+  "photo_Cover":"https://m.media-amazon.com/images/I/71F9xOlmd3L._AC_UF1000,1000_QL80_.jpg"
+
+},
+{
+  "id":"4",
+  "title": "Wakanda Forever",
+  "description": "Queen Ramonda (Angela Bassett), Shuri (Letitia Wright), M’Baku (Winston Duke), Okoye (Danai Gurira) and the Dora Milaje (including Florence Kasumba), fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia (Lupita Nyong’o) and Everett Ross (Martin Freeman) and forge a new path for the kingdom of Wakanda.",
+  "photo_Cover":"https://musicart.xboxlive.com/7/687d6400-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+
+}
+]
